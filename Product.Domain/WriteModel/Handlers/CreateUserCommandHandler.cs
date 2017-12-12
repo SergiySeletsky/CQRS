82f73d904c1eb29724ffe6d2b64ea903792a340f
Sergey Seletsky <SeletskySergey@live.com>
2017-12-12 13:22:02 +0200

    added test command handler

---
+++ Product.Domain/WriteModel/Handlers/CreateUserCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using Product.Domain.Core.Commands;
+using Product.Domain.WriteModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Product.Domain.WriteModel.Handlers
+{
+    public class CreateUserCommandHandler : ICommandHandler<CreateUserCommand>
+    {
+        public Task Handle(CreateUserCommand message, CancellationToken token = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
